32d638b42cff5bcd2cd238c7c1416aeee0bf6dd9
“Feggens <fmonbrun@yahoo.com>
2022-10-08 19:07:36 -0400

    some changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn project.wsgi --log-file -
+release: python3 manage.py migrate
--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
+
+
+django_heroku.settings(locals())
 
 env = environ.Env()
 # reading .env file
