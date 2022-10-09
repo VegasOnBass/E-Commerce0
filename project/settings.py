2ef05e3c236f7229e55788615528d0aec77da574
“Feggens <fmonbrun@yahoo.com>
2022-10-08 23:47:09 -0400

    some changes

--- project/settings.py
+++ project/settings.py
@@ -14,9 +14,10 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
